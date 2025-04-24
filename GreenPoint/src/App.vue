c764b73e67884bcdaec88667721c9ba26a2b3b8f
whatt20 <scarlbenedict@gmail.com>
2025-04-24 17:26:55 +0800

    vue worthy

--- GreenPoint/src/App.vue
+++ GreenPoint/src/App.vue
@@ -1,47 +1,60 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="header">
+    <p>GreenPoint</p>
+  </div>
+  <div class="chatbox">
+    <div class="chatbox-description">
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+export default {
+  name: 'HomePage',
+  components: {
+    
   }
 }
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Iceberg&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+.header {
+  width: 100%;
+  padding: 15px 30px;
+  background-color: #0A5E36;
+  color: white;
+  display: flex;
+  justify-content: space-between;
+  margin: 0px;
+}
+
+body{
+  background-color: #FBF6E9;
+  font-family: "Iceberg", sans-serif;
+  font-weight: 400;
+  font-size: 25px;
+  font-style: normal;
+}
+.chatbox{
+  padding: 10px;
+  height: 100px;
+  border-radius: 6px;
+  margin: 40px auto; 
+  width: 90%;
+  max-width: 500px; 
+  background: #E3F0AF; 
+  box-shadow: 10px 7px 40px -15px black ;
+}
+.chatbox-description{
+  padding: 10px;
+}
+
+
 </style>
