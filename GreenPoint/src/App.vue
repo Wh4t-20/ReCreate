2451abb4e7bf25fe65a02de981a0e124b4617471
Angelo Mari H. Manlangit <manlangitangelomh@gmail.com>
2025-04-29 18:43:20 +0800

    The light mode version of the index, still needs some work to be done

--- GreenPoint/src/App.vue
+++ GreenPoint/src/App.vue
@@ -1,26 +1,56 @@
-  <template>
+<template>
   <div id="app">
-      <div class="header">
-        GreenPoint
+    <header>
+      <img id="logo" src="https://static.wikia.nocookie.net/plantsvszombies/images/8/86/Melonpult2009HD.png/revision/latest?cb=20170909070525">
+      <h3>GreenPoint</h3>
+      <div class="theme-button">
+        <img id="moon" src="/Moon.png">
+      </div>        
+    </header>
+
+    <section>
+      <div class="column">
+        <h1 id="header-1">Are your crops good?</h1>
+        <p id="bottom-text">
+          Check if your land is suitable for growing a certain crop. 
+          You get an in-depth analysis of the land with optimization 
+          options.
+        </p>
       </div>
+
       <div class="chat">
-        <p class="h1">GreenPoint</p>
-        <p class="text1">What ails you yung blud?</p>
-        
+        <p class="big-text">GreenPoint</p>
+          
+        <p class="reg-text">What ails you yung blud?</p>
         <textarea 
           class="chat-description" 
           placeholder="Describe your plans"
           v-model="plan"
           required
+        ></textarea>
+          
+        <p class="reg-text">Plant</p>
+        <textarea 
+          class="smaller-description" 
+          placeholder="Enter a plant"
+          v-model="plan"
+          required
+        ></textarea>
+          
+        <p class="reg-text">Location</p>
+        <textarea 
+          class="smaller-description" 
+          placeholder="Enter a plant"
+          v-model="plan"
+          required
         ></textarea> 
-        <button class="analyze-button" @click="test">Analyze</button>
+        <button class="analyze-button">Analyze</button>
       </div>
+    </section>
   </div>
+</template>
 
-  </template>
-
-  <script>
-
+<script>
   export default {
     name: 'HomePage',
     components: {
@@ -33,7 +63,7 @@
     },
   },
 };
-  </script>
+</script>
 
-  <style>
-  </style>
+<style>
+</style>
